efb1f808ab5d403c279680c1444b182f39cab474
URVPenguin <gerard.casass@estudiants.urv.cat>
2024-12-24 09:49:15 +0100

    Test minecraft service

--- tests/test_minecraft_service.py
+++ tests/test_minecraft_service.py
@@ -7,31 +7,19 @@ mm = MagicMock()
 
 @pytest.fixture
 def mock_minecraft_create():
-    """Fixture para mockear minecraft.Minecraft.create"""
     with patch.object(minecraft.Minecraft, 'create', return_value=mm) as mock_create:
         yield mock_create
 
 def test_singleton_instance(mock_minecraft_create):
-    # Crear una instancia del servicio
     service1 = MinecraftService()
     service2 = MinecraftService()
-
-    # Verificamos que ambas instancias sean la misma
     assert service1 is service2, "MinecraftService should be a singleton"
-
-    # Verificamos que la conexión a Minecraft se haya creado solo una vez
     mock_minecraft_create.assert_called_once()
 
 def test_minecraft_connection(mock_minecraft_create):
-    # Crear la instancia del servicio
     service = MinecraftService()
-
-    # Obtener la instancia mockeada de Minecraft
     mock_mc_instance = mock_minecraft_create.return_value
-
-    # Verificamos que la instancia de Minecraft devuelta es la correcta
     mc_instance = service.get_instance()
     assert mc_instance is mock_mc_instance
-
     mc_instance.postToChat("Message from Minecraft")
     mock_mc_instance.postToChat.assert_called_once()
