09c3acdaeb9e612e5e647e9b0f52efad539c7b04
URVPenguin <gerard.casass@estudiants.urv.cat>
2024-12-27 12:57:27 +0100

    Add threading to chat_bot

--- src/agents/chat_bot.py
+++ src/agents/chat_bot.py
@@ -3,6 +3,7 @@ from core.events.chat_event_adapter import ChatEventAdapter
 from core.events.default_event_handler import DefaultEventHandler
 from hugchat.login import Login
 from hugchat import hugchat
+import threading
 
 sign = Login("gamec11123@chosenx.com", "n;>;V6?*fq]VWx9")
 cookies = sign.login(cookie_dir_path="./.cookies/", save_cookies=True)
@@ -12,9 +13,13 @@ def ask(question):
     message_result = chatbot.chat(question)
     return message_result.wait_until_done()[:150].encode('cp437', errors='replace').decode('cp437', errors='replace')
 
+def process_ask(event: ChatEventAdapter, agent: Agent):
+    response = ask(event.get_data())
+    agent.minecraft.send_message(response)
+
 class CustomEventHandler(DefaultEventHandler):
     def handle_chat_event(self, event: ChatEventAdapter, agent: Agent):
-        agent.minecraft.send_message(ask(event.get_data()))
+        threading.Thread(target=process_ask, args=(event, agent)).start()
 
 def create_agent():
     return Agent("ChatBot", None, CustomEventHandler())
